d47f6396cfcfb25d87c0d0cbefbb99df34263789
MartinMedinaRuvian <martindjmedina@hotmail.com>
2021-08-16 11:13:42 -0500

    modifique diseño de nueva factura

--- src/views/facturas/fac_guardar.vue
+++ src/views/facturas/fac_guardar.vue
@@ -1,102 +1,119 @@
 <template>
   <div class="mt-5 container">
-    <div class="card">
-      <div class="card-header">
-        <h6>Información basica</h6>
-      </div>
+    <div class="mt-5">
+      <div class="row">
+        <div class="col-md-4">
 
-      <div class="card-body">
-        <div class="mb-3">
-          <input
-            type="text"
-            placeholder="Nombre cliente"
-            class="form-control"
-            v-model="infoBasica.fac_nombrecliente"
-          />
+          <div class="card">
+            <div class="card-header">
+              <h6>Agregar producto</h6>
+            </div>
+
+            <div class="card-body">
+              <form
+                @submit.prevent="agregarArticulo(articulo), calcularTotal()"
+              >
+                <div class="mb-3">
+                  <input
+                    type="text"
+                    placeholder="Descripción articulo"
+                    class="form-control"
+                    v-model="articulo.descripcion"
+                    required
+                    id="txtdescipcion"
+                  />
+                </div>
+                <div class="mb-3">
+                  <input
+                    type="number"
+                    placeholder="Cantidad"
+                    class="form-control"
+                    v-model="articulo.cantidad"
+                    required
+                  />
+                </div>
+                <div class="mb-3">
+                  <input
+                    type="number"
+                    placeholder="Valor"
+                    class="form-control"
+                    v-model="articulo.valor"
+                    required
+                  />
+                </div>
+                <div class="mb-3">
+                  <input
+                    type="submit"
+                    value="Agregar producto"
+                    class="form-control btn btn-primary"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <div class="mb-3">
-          <input
-            type="date"
-            placeholder="Fecha"
-            class="form-control"
-            v-model="infoBasica.fac_fecha"
-          />
+
+        <div class="col-md-8">
+          <table class="table table-center table-primary">
+            <thead>
+              <tr>
+                <th scope="col">Articulo</th>
+                <th scope="col">Cantidad</th>
+                <th scope="col">Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="dato of articulos" :key="dato.index">
+                <td>{{ dato.descripcion }}</td>
+                <td>{{ dato.cantidad }}</td>
+                <td>$ {{ dato.valor }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
-    </div>
 
-    <div class="card mt-3">
-      <div class="card-header">
-        <h6>Agregar producto</h6>
-      </div>
+      <div class="row mt-5">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-header">
+              <h6>Información basica</h6>
+            </div>
 
-      <div class="card-body">
-        <form @submit.prevent="agregarArticulo(articulo), calcularTotal()">
-          <div class="mb-3">
-            <input
-              type="text"
-              placeholder="Descripción articulo"
-              class="form-control"
-              v-model="articulo.descripcion"
-            />
+            <div class="card-body">
+              <div class="mb-3">
+                <input
+                  type="text"
+                  placeholder="Nombre cliente"
+                  class="form-control"
+                  v-model="infoBasica.fac_nombrecliente"
+                />
+              </div>
+              <div class="mb-3">
+                <input
+                  type="date"
+                  placeholder="Fecha"
+                  class="form-control"
+                  v-model="infoBasica.fac_fecha"
+                />
+              </div>
+            </div>
           </div>
-          <div class="mb-3">
-            <input
-              type="number"
-              placeholder="Cantidad"
-              class="form-control"
-              v-model="articulo.cantidad"
-            />
+        </div>
+
+        <div class="col-md-6">
+          <div class="btn btn-outline-primary p-3 disabled">
+            <h4>Sub total: $ {{ formatoMoneda(subtotal) }}</h4>
+            <h4>IVA: $ {{ formatoMoneda(iva_total) }}</h4>
+            <h3>Total: $ {{ formatoMoneda(total) }}</h3>
           </div>
-          <div class="mb-3">
-            <input
-              type="number"
-              placeholder="Valor"
-              class="form-control"
-              v-model="articulo.valor"
-            />
-          </div>
-          <div class="mb-3">
-            <input
-              type="submit"
-              value="Agregar producto"
-              class="form-control btn btn-primary"
-            />
-          </div>
-        </form>
+        </div>
       </div>
 
       <div class="mt-5">
-        <h4>Articulos agregados:</h4>
-
-        <table class="table table-center table-primary">
-          <thead>
-            <tr>
-              <th scope="col">Articulo</th>
-              <th scope="col">Cantidad</th>
-              <th scope="col">Valor</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="dato of articulos" :key="dato.index">
-              <td>{{ dato.descripcion }}</td>
-              <td>{{ dato.cantidad }}</td>
-              <td>$ {{ dato.valor }}</td>
-            </tr>
-          </tbody>
-        </table>
-
-        <div class="mt-4">
-          <h4>Sub total: ${{ subtotal }}</h4>
-          <h4>IVA: ${{ iva_total }}</h4>
-          <h3>Total: ${{ total }}</h3>
-        </div>
-
-        <div class="mt-5">
-          <button class="btn btn-success" @click="guardarFactura(infoBasica)">
-            Guardar factura
-          </button>
-        </div>
+        <button class="btn btn-success" @click="guardarFactura(infoBasica)">
+          Guardar factura
+        </button>
       </div>
     </div>
   </div>
@@ -120,6 +137,7 @@ export default {
     agregarArticulo(articulo) {
       this.articulos.push(articulo);
       this.articulo = {};
+      document.querySelector('#txtdescipcion').focus();
     },
     calcularTotal() {
       let suma = 0;
@@ -129,6 +147,9 @@ export default {
       this.subtotal = suma;
       this.iva_total = this.subtotal * this.iva;
       this.total = this.subtotal + this.iva_total;
+    },
+    formatoMoneda(numero){
+      return new Intl.NumberFormat().format(numero);
     },
     guardarFactura(infoBasica) {
       const { fac_nombrecliente, fac_fecha } = infoBasica;
